8ed6bb2e2d5c42741902a01913582f86fca37e22
JHJH1289 <wogns4631298@gmail.com>
2026-06-18 09:57:05 +0900

    Remove H2 test datasource override

--- backend/src/test/java/com/stockviewer/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/stockviewer/backend/BackendApplicationTests.java
@@ -3,14 +3,7 @@ package com.stockviewer.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {
-		"spring.datasource.driver-class-name=org.h2.Driver",
-		"spring.datasource.url=jdbc:h2:mem:stockviewer;MODE=Oracle;DB_CLOSE_DELAY=-1",
-		"spring.datasource.username=sa",
-		"spring.datasource.password=",
-		"spring.jpa.hibernate.ddl-auto=create-drop",
-		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
-})
+@SpringBootTest
 class BackendApplicationTests {
 
 	@Test
